3e26505489a00a89d5260439f4157a6712aca59c
RoccoCerro <rcerro02@gmail.com>
2024-03-07 16:13:36 +0100

    FEAT gioco dei dati

--- gioco-dei-dati/js/app.js
+++ gioco-dei-dati/js/app.js
@@ -11,10 +11,24 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
     // IF (giocatore > computer){
         // console.log(Vince il giocatore)
-    // }
+    // } else if (giocatore = computer)
+        // console.log(pari) 
     // else
     // {
         // console.log(vince il pc)
     // }
 
-    
+    let giocatore = Math.floor(Math.random() * 6) + 1
+    console.log ("Giocatore ", giocatore)
+    let computer = Math.floor(Math.random() * 6) + 1
+    console.log ("Computer ", computer)
+
+    if (giocatore > computer){
+        console.log("Hai vinto")
+    }
+    else if (giocatore === computer) {
+        console.log("Pari")
+    }
+    else {
+        console.log("Hai perso")
+    }
